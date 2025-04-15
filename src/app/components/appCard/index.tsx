498cd661be81dacd531e361ff39dc2f139af1ac5
Tooba Afzali <toobaafzalii78@gmail.com>
2025-04-15 12:52:25 +0330

    feat: header component -ui

--- src/app/components/appCard/index.tsx
+++ src/app/components/appCard/index.tsx
@@ -29,18 +29,16 @@ export function AppCard() {
         <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">TASK TITLE</h5>
         <span className="text-sm text-gray-500 dark:text-gray-400">descroption</span>
         <div className="mt-4 flex space-x-3 lg:mt-6">
-          <a
-            href="#"
+          <button
             className="inline-flex items-center rounded-lg bg-cyan-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
           >
             Done
-          </a>
-          <a
-            href="#"
+          </button>
+          <button
             className="inline-flex items-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-red-500 dark:text-white dark:hover:border-gray-700 dark:hover:bg-red-700 dark:focus:ring-gray-700"
           >
             Delete
-          </a>
+          </button>
         </div>
       </div>
     </Card>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+
 import AppCard from "./components/appCard";
+import AppHeader from "./components/appHeader";
 
 export default function App() {
   return (
-    <div className="container mx-auto p-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4">
+    <div className="w-full mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4">
+      <AppHeader/>
+      <AppCard/>
+      <AppCard/>
+      <AppCard/>
     </div>
   );
 }
