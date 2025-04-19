f75c4c53b583239d798c6c2720199c5459df79b5
Tooba Afzali <toobaafzalii78@gmail.com>
2025-04-20 00:01:28 +0330

    feat: add dropdown component

--- src/app/components/appHeader/index.tsx
+++ src/app/components/appHeader/index.tsx
@@ -1,123 +1,157 @@
-"use client"
+"use client";
 
-import React from "react";
-import {
-    Button,
-    MegaMenu,
-    NavbarBrand,
-    NavbarToggle,
-  } from "flowbite-react";
+import React, { useEffect, useRef, useState } from "react";
+import { Button, MegaMenu, NavbarBrand, NavbarToggle } from "flowbite-react";
 import AppAvatar from "../appAvatar";
+import AppDropdown from "../appDropdown";
 
-  const customTheme = {
-    "root": {
-      "base": "bg-white px-4 py-5 sm:px-4 dark:border-gray-700 dark:bg-gray-800",
-      "rounded": {
-        "on": "rounded",
-        "off": ""
+const customTheme = {
+  root: {
+    base: "bg-white px-4 py-5 sm:px-4 dark:border-gray-700 dark:bg-gray-800",
+    rounded: {
+      on: "rounded",
+      off: "",
+    },
+    bordered: {
+      on: "border",
+      off: "",
+    },
+    inner: {
+      base: "mx-auto flex flex-wrap items-center justify-between",
+      fluid: {
+        on: "",
+        off: "container",
       },
-      "bordered": {
-        "on": "border",
-        "off": ""
-      },
-      "inner": {
-        "base": "mx-auto flex flex-wrap items-center justify-between",
-        "fluid": {
-          "on": "",
-          "off": "container"
-        }
-      }
     },
-    "brand": {
-      "base": "flex items-center"
+  },
+  brand: {
+    base: "flex items-center",
+  },
+  collapse: {
+    base: "w-full md:block md:w-auto",
+    list: "mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium",
+    hidden: {
+      on: "hidden",
+      off: "",
     },
-    "collapse": {
-      "base": "w-full md:block md:w-auto",
-      "list": "mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium",
-      "hidden": {
-        "on": "hidden",
-        "off": ""
-      }
+  },
+  link: {
+    base: "block py-2 pl-3 pr-4 md:p-0",
+    active: {
+      on: "bg-primary-700 text-white md:bg-transparent md:text-primary-700 dark:text-white",
+      off: "border-b border-gray-100 text-gray-700 hover:bg-gray-50 md:border-0 md:hover:bg-transparent md:hover:text-primary-700 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-white",
     },
-    "link": {
-      "base": "block py-2 pl-3 pr-4 md:p-0",
-      "active": {
-        "on": "bg-primary-700 text-white md:bg-transparent md:text-primary-700 dark:text-white",
-        "off": "border-b border-gray-100 text-gray-700 hover:bg-gray-50 md:border-0 md:hover:bg-transparent md:hover:text-primary-700 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-white"
-      },
-      "disabled": {
-        "on": "text-gray-400 hover:cursor-not-allowed dark:text-gray-600",
-        "off": ""
-      }
+    disabled: {
+      on: "text-gray-400 hover:cursor-not-allowed dark:text-gray-600",
+      off: "",
     },
-    "toggle": {
-      "base": "inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600",
-      "icon": "h-6 w-6 shrink-0",
-      "title": "sr-only"
-    },
-    "dropdown": {
-      "base": "",
-      "toggle": {
-        "arrowIcon": "ml-2 h-4 w-4",
-        "content": "py-1 focus:outline-none",
-        "floating": {
-          "animation": "transition-opacity",
-          "arrow": {
-            "base": "absolute z-10 h-2 w-2 rotate-45",
-            "style": {
-              "dark": "bg-gray-900 dark:bg-gray-700",
-              "light": "bg-white",
-              "auto": "bg-white dark:bg-gray-700"
-            },
-            "placement": "-4px"
+  },
+  toggle: {
+    base: "inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600",
+    icon: "h-6 w-6 shrink-0",
+    title: "sr-only",
+  },
+  dropdown: {
+    base: "",
+    toggle: {
+      arrowIcon: "ml-2 h-4 w-4",
+      content: "py-1 focus:outline-none",
+      floating: {
+        animation: "transition-opacity",
+        arrow: {
+          base: "absolute z-10 h-2 w-2 rotate-45",
+          style: {
+            dark: "bg-gray-900 dark:bg-gray-700",
+            light: "bg-white",
+            auto: "bg-white dark:bg-gray-700",
           },
-          "base": "z-10 w-fit divide-y divide-gray-100 rounded shadow focus:outline-none mt-2 block",
-          "content": "py-1 text-sm text-gray-500 dark:text-gray-400",
-          "divider": "my-1 h-px bg-gray-100 dark:bg-gray-600",
-          "header": "block px-4 py-2 text-sm text-gray-700 dark:text-gray-200",
-          "hidden": "invisible opacity-0",
-          "item": {
-            "container": "",
-            "base": "flex w-full cursor-pointer items-center justify-start px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:bg-gray-600 dark:focus:text-white",
-            "icon": "mr-2 h-4 w-4"
-          },
-          "style": {
-            "dark": "bg-gray-900 text-white dark:bg-gray-700",
-            "light": "border border-gray-200 bg-white text-gray-900",
-            "auto": "border border-gray-200 bg-white dark:border-none dark:bg-gray-700 text-gray-500 dark:text-gray-400"
-          },
-          "target": "w-fit"
+          placement: "-4px",
         },
-        "inlineWrapper": "flex w-full items-center justify-between"
-      }
+        base: "z-10 w-fit divide-y divide-gray-100 rounded shadow focus:outline-none mt-2 block",
+        content: "py-1 text-sm text-gray-500 dark:text-gray-400",
+        divider: "my-1 h-px bg-gray-100 dark:bg-gray-600",
+        header: "block px-4 py-2 text-sm text-gray-700 dark:text-gray-200",
+        hidden: "invisible opacity-0",
+        item: {
+          container: "",
+          base: "flex w-full cursor-pointer items-center justify-start px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:bg-gray-600 dark:focus:text-white",
+          icon: "mr-2 h-4 w-4",
+        },
+        style: {
+          dark: "bg-gray-900 text-white dark:bg-gray-700",
+          light: "border border-gray-200 bg-white text-gray-900",
+          auto: "border border-gray-200 bg-white dark:border-none dark:bg-gray-700 text-gray-500 dark:text-gray-400",
+        },
+        target: "w-fit",
+      },
+      inlineWrapper: "flex w-full items-center justify-between",
     },
-    "dropdownToggle": {
-      "base": "py-2 pl-3 pr-4 md:p-0 border-b border-gray-100 text-gray-700 hover:bg-gray-50 md:border-0 md:hover:bg-transparent md:hover:text-primary-700 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-white flex w-full items-center justify-between"
-    }
-  }
-  
-  interface AppHeaderProps {
-    onNewTaskClick: () => void;
-  }
-  
-  const AppHeader:React.FC<AppHeaderProps> =  ({ onNewTaskClick }: AppHeaderProps)=> {
-    return (
-      <MegaMenu theme={customTheme} className="w-full col-span-4">
-        <NavbarBrand href="/">
-          <span className="self-center whitespace-nowrap text-2xl sm:text-3xl font-semibold dark:text-white">
-            TASK MANAGER
-          </span>
-        </NavbarBrand>
-        <div className="order-2 items-center gap-2 flex cursor-pointer">
-          <div className="flex justify-center gap-1 items-center">
-            <AppAvatar src={"https://picsum.photos/200"} />
-            <img src="/dropdown-arrow.svg" className="w-4 h-4 hover:scale-110" />
-          </div>
-          <Button className="cursor-pointer" onClick={onNewTaskClick}>New Task</Button>
+  },
+  dropdownToggle: {
+    base: "py-2 pl-3 pr-4 md:p-0 border-b border-gray-100 text-gray-700 hover:bg-gray-50 md:border-0 md:hover:bg-transparent md:hover:text-primary-700 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-white flex w-full items-center justify-between",
+  },
+};
+
+interface AppHeaderProps {
+  onNewTaskClick: () => void;
+}
+
+const AppHeader: React.FC<AppHeaderProps> = ({
+  onNewTaskClick,
+}: AppHeaderProps) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        // setDropdownOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  const onAvatarClick = () => {
+    setDropdownOpen(true);
+  };
+
+  return (
+    <MegaMenu theme={customTheme} className="w-full col-span-4 relative">
+      <NavbarBrand href="/">
+        <span className="self-center whitespace-nowrap text-2xl sm:text-3xl font-semibold dark:text-white">
+          TASK MANAGER
+        </span>
+      </NavbarBrand>
+      <div className="order-2 items-center gap-2 flex cursor-pointer">
+        <div
+          onClick={onAvatarClick}
+          ref={dropdownRef}
+          className="flex justify-center gap-1 items-center"
+        >
+          <AppAvatar src={"https://picsum.photos/200"} />
+          <img
+            src="/dropdown-arrow.svg"
+            className={`w-4 h-4 hover:scale-110 transition-transform ${
+              dropdownOpen ? "rotate-180" : ""
+            }`}
+          />
         </div>
-        {/* <NavbarToggle /> */}
-      </MegaMenu>
-    );
-  }
-  
-  export default AppHeader;
+        <Button className="cursor-pointer" onClick={onNewTaskClick}>
+          New Task
+        </Button>
+      </div>
+      {/* <NavbarToggle /> */}
+      {dropdownOpen && (
+        <div className="absolute top-16 right-16 mt-2 z-50">
+          <AppDropdown />
+        </div>
+      )}
+    </MegaMenu>
+  );
+};
+
+export default AppHeader;
